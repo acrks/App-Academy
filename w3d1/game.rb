225813f2602b682e0fc6e74f68ad7b72c49ced9f
Alex Crooks <alexcrooks2@gmail.com>
2021-07-19 18:06:55 -0700

    Added enumerables

--- w3d1/game.rb
+++ w3d1/game.rb
@@ -1,7 +1,18 @@
 class Game
-    def initialize(player_1, player_2)
-        @dictionary = []
-        @fragment 
+    attr_reader :dictionary
+    def initialize
+        @dictionary = read_in_dict
+        @fragment = []
+    end
+
+    def read_in_dict
+        hash = {}
+        file = File.open("dictionary.txt")
+        file_data = file.readlines.map(&:chomp)
+        file_data.each do |i|
+            hash[i] = 1
+        end
+        hash
     end
 
     def play_round
@@ -20,7 +31,7 @@ class Game
     end
 
     def valid_play?(string)
-        alpha = ('a'..'z').to_a?(Array)
+        alpha = ('a'..'z').to_a
         return alpha.include?(string)
         # Checks that string is a letter of the alphabet and that there are words we can spell after 
         # adding it to the fragment
@@ -33,6 +44,5 @@ end
 #previous_player
 #next_player!: updates the current_player and previous_player
 #take_turn(player): gets a string from the player until a valid play is made; 
-then updates the fragment and checks against the dictionary. You may also want to alert the player 
-if they attempt to make an invalid move (or, if you're feeling mean, you might cause them to lose outright).
-#valid_play?(string): 
+# then updates the fragment and checks against the dictionary. You may also want to alert the player 
+# if they attempt to make an invalid move (or, if you're feeling mean, you might cause them to lose outright).
