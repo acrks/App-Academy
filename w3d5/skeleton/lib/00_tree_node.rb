fbce61b4cad4363efe74ea0d918856811fb96fed
atjohnfeng <atjohnfeng@gmail.com>
2021-07-23 15:41:30 -0700

    Solved DFS with magic

--- w3d5/skeleton/lib/00_tree_node.rb
+++ w3d5/skeleton/lib/00_tree_node.rb
@@ -7,51 +7,54 @@ class PolyTreeNode
         @children = children
     end
 
-    # def inspect
-    #     p value, parent, children
-    # end
+    def inspect
+        p "#Node: #{self.object_id}"
+    end
 
     def parent=(new_parent)
         
         @parent.children.delete(self) if @parent != nil
-        # new_parent.parent=(nil) if @parent == nil #root
         if new_parent != nil && !new_parent.children.include?(self)
             new_parent.children << self 
         end
         @parent = new_parent
     end
 
-    # A
-    # B
+    def add_child(child)
+        child.parent = self
+    end
 
-    # Changing A to be child of B
+    def remove_child(child)
+        raise "#{child} is not a child of this node" if !@children.include?(child)
+        child.parent = nil
+    end
 
-    # B
-    # A
-
-    # old parent == nil
-    # new parent = B
-    # remove B as child of A
-    # add A as child of B
-
-
-
-    # def add_child(child)
-    #     if !children.include?(child) && !child.nil?
-    #         children << child
-    #     end
-    # end
-
-    # def remove_child
+    def dfs(target)
+        return self if @value == target 
+            
+        @children.each do |child|
+            child_search = child.dfs(target)
+            if child_search != nil
+                return child_search
+            end
+        end 
         
-    # end
+        return nil
+    end
 
-    #A
-    #B 
-    #C
-    # NodeB
-    #Node C: Parent: B, Children: []
-    #Node B: Parent: A, Children [C]
+    # Depth First Search
+    # 1) Check if node is target
+    # 2) Iterate over node's children
+    #    a) Call DFS on child
+    #    b) if result != nil, return node
+    # 3) return nil
 
+    # Breadth First Search
+    # 1) Create queue with root node
+    # 2) Until queue empty
+    #     a) dequeue node
+    #     b) check if node matches target
+    #     c) if not, add children to queue
+    # 3) if queue empty, return nil
 
 end
