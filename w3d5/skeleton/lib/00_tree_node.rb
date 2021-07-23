fe98e0b2b3f0a8fa2226dd88398fb4e7781df085
atjohnfeng <atjohnfeng@gmail.com>
2021-07-23 14:13:27 -0700

    added additional lines

--- w3d5/skeleton/lib/00_tree_node.rb
+++ w3d5/skeleton/lib/00_tree_node.rb
@@ -8,11 +8,30 @@ class PolyTreeNode
     end
 
     def parent=(new_parent)
-        @parent = new_parent
-        @children.each do |i|
-            new_parent.children += i
-        end
+        # old_parent = @parent
+        # old_parent.children.delete(self)
+        # if new_parent != nil
+            @parent = new_parent
+            new_parent.children << self 
+        # end
     end
+
+    # def add_child(child)
+    #     if !children.include?(child) && !child.nil?
+    #         children << child
+    #     end
+    # end
+
+    # def remove_child
+        
+    # end
+
+    #A
+    #B 
+    #C
+    # NodeB
+    #Node C: Parent: B, Children: []
+    #Node B: Parent: A, Children [C]
 
 
 end
