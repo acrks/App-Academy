dbea4a22572a0d8498789ad64736552358cede2f
Alex Crooks <alexcrooks2@gmail.com>
2021-07-23 15:00:59 -0700

    Solved this bloody parent= issue

--- w3d5/skeleton/lib/00_tree_node.rb
+++ w3d5/skeleton/lib/00_tree_node.rb
@@ -7,14 +7,34 @@ class PolyTreeNode
         @children = children
     end
 
+    # def inspect
+    #     p value, parent, children
+    # end
+
     def parent=(new_parent)
-        # old_parent = @parent
-        # old_parent.children.delete(self)
-        # if new_parent != nil
-            @parent = new_parent
+        
+        @parent.children.delete(self) if @parent != nil
+        # new_parent.parent=(nil) if @parent == nil #root
+        if new_parent != nil && !new_parent.children.include?(self)
             new_parent.children << self 
-        # end
+        end
+        @parent = new_parent
     end
+
+    # A
+    # B
+
+    # Changing A to be child of B
+
+    # B
+    # A
+
+    # old parent == nil
+    # new parent = B
+    # remove B as child of A
+    # add A as child of B
+
+
 
     # def add_child(child)
     #     if !children.include?(child) && !child.nil?
