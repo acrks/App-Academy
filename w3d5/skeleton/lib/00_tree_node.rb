60108a30b05956afd567e3a1c1af72c3a8bc0c10
Alex Crooks <alexcrooks2@gmail.com>
2021-07-23 13:04:53 -0700

    polytreenode init

--- w3d5/skeleton/lib/00_tree_node.rb
+++ w3d5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,18 @@
 class PolyTreeNode
+    attr_reader :value, :parent, :children
+
+    def initialize(init_value, parent = nil, children = [])
+        @value = init_value
+        @parent = parent
+        @children = children
+    end
+
+    def parent=(new_parent)
+        @parent = new_parent
+        @children.each do |i|
+            new_parent.children << i
+        end
+    end
+
 
 end
