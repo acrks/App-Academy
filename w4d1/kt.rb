3b6dde74d680f857c8c8ae2925ce96309167718f
atjohnfeng <atjohnfeng@gmail.com>
2021-07-26 11:20:06 -0700

    Added valid_moves method

--- w4d1/kt.rb
+++ w4d1/kt.rb
@@ -1,7 +1,26 @@
 require_relative "00_tree_node"
 
 class KnightPathFinder
-    
+
+    def initialize(pos)
+        self.root_node = PolyTreeNode.new(pos)
+        @considered_positions = [pos]
+    end
+
+    def self.valid_moves(pos)
+        row, col = pos 
+        if (row == 2 && col == 1) || (row == 1 && col == 2)
+            return true
+        else
+            return false
+        end
+    end
+
+    def build_move_tree
+
+    end
+
+    # TODO: write method build_move_tree
 end
 
 kpf = KnightPathFinder.new([0, 0])
