aac3ad0984f4cecb70d33fbd61e8041a48f9a276
Alex Crooks <alexcrooks2@gmail.com>
2021-07-26 10:17:07 -0700

    update for w4d1

---
+++ w4d1/skeleton/lib/00_tree_node.rb
@@ -0,0 +1,70 @@
+class PolyTreeNode
+    attr_reader :value, :parent, :children
+
+    def initialize(init_value, parent = nil, children = [])
+        @value = init_value
+        @parent = parent
+        @children = children
+    end
+
+    def inspect
+        p "#Node: #{self.object_id}"
+    end
+
+    def parent=(new_parent)
+        
+        @parent.children.delete(self) if @parent != nil
+        if new_parent != nil && !new_parent.children.include?(self)
+            new_parent.children << self 
+        end
+        @parent = new_parent
+    end
+
+    def add_child(child)
+        child.parent = self
+    end
+
+    def remove_child(child)
+        raise "#{child} is not a child of this node" if !@children.include?(child)
+        child.parent = nil
+    end
+
+    def dfs(target)
+        return self if @value == target 
+            
+        @children.each do |child| 
+            child_search = child.dfs(target)
+            return child_search if child_search
+        end 
+        
+        return nil
+    end
+
+    # Depth First Search
+    # 1) Check if node is target
+    # 2) Iterate over node's children
+    #    a) Call DFS on child
+    #    b) if result != nil, return node
+    # 3) return nil
+
+    def bfs(target)
+        arr = [self]
+        until arr.empty?
+            node = arr.shift
+            if node.value == target
+                return node
+            else
+                node.children.each { |child| arr << child }
+            end
+        end
+        return nil
+    end
+    # Breadth First Search
+    # 1) Create queue with root node
+    # 2) Until queue empty
+    #     a) dequeue node
+    #     b) check if node matches target
+    #     c) if not, add children to queue
+    # 3) if queue empty, return nil
+
+end
