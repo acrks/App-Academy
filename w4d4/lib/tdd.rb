6bc59f6ddd8c929a0adcb8bbe8ffc9d3384f55a6
David Oh <davidohyoung@gmail.com>
2021-07-29 16:57:43 -0500

    finished stock picker

--- w4d4/lib/tdd.rb
+++ w4d4/lib/tdd.rb
@@ -16,5 +16,41 @@ class Array
         end
         new_arr.sort
     end
+
+    def my_transpose
+        new_arr = Array.new(self.length) {Array.new(self.length)}
+        (0..self.length - 1).each do |row|
+            (0..self.length - 1).each do |ele| 
+                new_arr[ele][row] = self[row][ele]
+            end
+        end
+        new_arr
+    end
+    
+    # def my_transpose
+    #     new_arr = []
+    #     (0..self.length - 1).each do |row|
+    #         temp = []
+    #         (0..self.length - 1).each do |ele| 
+    #             temp << self[ele][row]
+    #         end
+    #         new_arr << temp
+    #     end
+    #     new_arr
+    # end
+
+    def stock_picker 
+        new_arr = []
+        buy = self.index(self.min)
+        new_arr << buy
+
+        sell = self.index(self[buy..-1].max)
+        new_arr << sell
+
+        new_arr
+    end
+
+    
+    
 end
 
--- w4d4/spec/tdd_spec.rb
+++ w4d4/spec/tdd_spec.rb
@@ -16,3 +16,27 @@ describe "#two_sum" do
         expect([-1, 0, 2, -2, 1].reverse.two_sum).to eq([[0, 4], [1, 2]])
     end
 end
+
+describe "#my_transpose" do
+    it "will convert between the row-oriented and column-oriented representations" do
+        expect([
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]
+        ].my_transpose).to eq([
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8]
+          ])
+    end
+end
+
+describe "#stock_picker" do
+    it "will output the most profitable pair of days on which to first buy stock, then sell the stock" do
+        expect([2,3,1,4,1,5].stock_picker).to eq([2,5])
+    end
+    it "will output the most profitable pair of days because you minimize losses" do
+        expect([6,5,3,1].stock_picker).to eq([6,5])
+    end
+
+end
