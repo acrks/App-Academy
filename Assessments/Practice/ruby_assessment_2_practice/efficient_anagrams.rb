85745495e0594febc360ff5a7e2242d71301adb2
Alex Crooks <alexcrooks2@gmail.com>
2021-07-29 22:11:45 -0700

    CLeaning house

---
+++ Assessments/Practice/ruby_assessment_2_practice/efficient_anagrams.rb
@@ -0,0 +1,23 @@
+# Write a method `anagrams(str_1, str_2)` that takes in two strings and returns
+# a boolean indicating whether or not the two strings are anagrams. Two strings are
+# anagrams if they contain the same exact characters, but not necessarily in the
+# same order.
+#
+# Your solution should have a linear - O(n) runtime, where n is the combined
+# length of the input strings.
+#
+# Scoring: 8 points maximum
+#   4 points awarded for passing all seven test cases
+#   4 points awarded for linear runtime
+#
+# This component of the assessment is estimated to take 10 minutes.
+
+
+# Test Cases
+p anagrams("restful", "fluster")    # => true
+p anagrams("zyyx", "zyxy")          # => true
+p anagrams("base", "case")          # => false
+p anagrams("taco", "cat")           # => false
+p anagrams("cat", "taco")           # => false
+p anagrams("zyyx", "zyx")           # => false
+p anagrams("zyx", "zyyx")           # => false
