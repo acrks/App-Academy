b08f492ceaea1bb761d2d2ec1ecf007828e96384
Alex Crooks <alexcrooks2@gmail.com>
2021-07-30 12:13:00 -0700

    Phase 2 largest_contiguous_sum started

--- Classwork/w4/w4d5/timedifference.rb
+++ Classwork/w4/w4d5/timedifference.rb
@@ -45,13 +45,35 @@ end
 list = [5, 3, -7]
 # p largest_contiguous_subsum(list) # => 8
 
-# possible sub-sums
-# [5]           # => 5
-# [5, 3]        # => 8 --> we want this one
-# [5, 3, -7]    # => 1
-# [3]           # => 3
-# [3, -7]       # => -4
-# [-7]          # => -7
+# Example 2:
+list = [2, 3, -6, 7, -6, 7]
+# p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+
+# Example 3:
+list = [-5, -1, -3]
+# p largest_contiguous_subsum(list) # => -1 (from [-1])
+
+def largest_contiguous_subsum2(arr)
+  temp_val = []
+  last = 0
+  (0...arr.length).each do |start|
+    (start...arr.length).each do |last|
+      temp_val << arr[start..last]
+    end
+  end
+  variable = temp_val.first.sum
+  temp_val.each do |a|
+    variable = a.sum if a.sum > variable
+  end
+  return variable
+end
+
+# Let's make a better version. Write a new function using O(n) time with O(1) memory. 
+# Keep a running tally of the largest sum. To accomplish this efficient space complexity, consider using two variables. 
+# One variable should track the largest sum so far and another to track the current sum. We'll leave the rest to you.
+
+list = [5, 3, -7]
+p largest_contiguous_subsum2(list) # => 8
 
 # Example 2:
 list = [2, 3, -6, 7, -6, 7]
@@ -60,3 +82,5 @@ list = [2, 3, -6, 7, -6, 7]
 # Example 3:
 list = [-5, -1, -3]
 # p largest_contiguous_subsum(list) # => -1 (from [-1])
+
+
