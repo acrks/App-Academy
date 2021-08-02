b7b2a0ad61a669a3e5c5fe9a5e78743b2a2c3235
Alex Crooks <alexcrooks2@gmail.com>
2021-08-02 14:07:03 -0700

    Phase one solved

--- Classwork/w5/w5d1/skeleton/lib/p01_int_set.rb
+++ Classwork/w5/w5d1/skeleton/lib/p01_int_set.rb
@@ -1,14 +1,21 @@
 class MaxIntSet
+  attr_reader :store
   def initialize(max)
+    @max = max
+    @store = Array.new(max) {false}
   end
 
   def insert(num)
+    raise 'Out of bounds' if num > @max || num < 0
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
