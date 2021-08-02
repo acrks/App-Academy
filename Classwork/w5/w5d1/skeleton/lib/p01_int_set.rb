f41b7e52cee8e550bb7d008dda0f5e06a75d2ae6
Josh Kim-Biggs <joshgykim@gmail.com>
2021-08-02 15:27:18 -0700

    Completed ResizingIntSet

--- Classwork/w5/w5d1/skeleton/lib/p01_int_set.rb
+++ Classwork/w5/w5d1/skeleton/lib/p01_int_set.rb
@@ -6,8 +6,8 @@ class MaxIntSet
   end
 
   def insert(num)
-    raise 'Out of bounds' if num > @max || num < 0
-    @store[num] = true
+    raise 'Out of bounds' if !is_valid?(num)
+    validate!(num)
   end
 
   def remove(num)
@@ -21,31 +21,39 @@ class MaxIntSet
   private
 
   def is_valid?(num)
+    num < @max && num >= 0
   end
 
   def validate!(num)
+    @store[num] = true
   end
 end
 
 
 class IntSet
+  attr_reader :store
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -62,18 +70,31 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if @count >= @store.length
+
+    if !include?(num)
+      @count += 1
+      self[num] << num
+      return true
+    end
   end
 
   def remove(num)
+    if include?(num)
+      self[num].clear
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -81,5 +102,15 @@ class ResizingIntSet
   end
 
   def resize!
+    new_arr = Array.new(num_buckets * 2) { Array.new }
+
+    @store.each do |bucket|
+      if !bucket.empty?
+        new_bucket_int = bucket.first % new_arr.length
+        new_arr[new_bucket_int] << bucket.first
+      end
+    end
+    @store = new_arr
   end
+
 end
