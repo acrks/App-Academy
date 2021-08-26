91cf44f0e9f9da65ca60f2c8800df320ed2df3c1
Alex Crooks <alexcrooks2@gmail.com>
2021-08-26 16:34:46 -0700

    myThrottle started

--- Classwork/w8/w8d4/intro_callback_exercises.js
+++ Classwork/w8/w8d4/intro_callback_exercises.js
@@ -131,5 +131,78 @@ function askIfGreaterThan(el1, el2, callback) {
 
   
 Function.prototype.myBind = function (context) {
-  return function (this, context)
+    
+    return () => {
+        this.apply(context);
+    }; 
 }
+
+class Lamp {
+    constructor() {
+      this.name = "a lamp";
+    }
+  }
+  
+  const turnOn = function() {
+    console.log("Turning on " + this.name);
+  };
+  
+  const lamp = new Lamp();
+  
+  turnOn(); // should not work the way we want it to
+  
+  const boundTurnOn = turnOn.bind(lamp);
+  const myBoundTurnOn = turnOn.myBind(lamp);
+  
+  boundTurnOn(); // should say "Turning on a lamp"
+  myBoundTurnOn(); // should say "Turning on a lamp"
+
+Function.prototype.myThrottle = function (interval) {
+    let tooSoon = false;
+    return function () {
+        if(!tooSoon) {
+            tooSoon = true;
+            setTimeout(function () {
+                tooSoon = false}, interval) //is this right?
+            this(interval)
+        }
+    }
+}
+
+
+  class Neuron {
+  fire() {
+    console.log("Firing!");
+  }
+}
+
+const neuron = new Neuron();
+// When we create a new Neuron,
+// we can call #fire as frequently as we want
+
+// Using Function#myThrottle, we should be able to throttle
+// the #fire function of our neuron so that it can only fire
+// once every 500ms:
+
+neuron.fire = neuron.fire.myThrottle(500);
+
+interval = setInterval(() => {
+  neuron.fire();
+}, 10);
+
+// This time, if our Function#myThrottle worked correctly,
+// the Neuron#fire function should only be able to execute
+// every 500ms, even though we're still trying to invoke it
+// every 10ms!
+
+// If we want this behavior for ALL neurons, we can do the same logic in the constructor:
+
+// class Neuron {
+//   constructor() {
+//     this.fire = this.fire.myThrottle(500);
+//   }
+
+//   fire() {
+//     console.log("Firing!");
+//   }
+// }
