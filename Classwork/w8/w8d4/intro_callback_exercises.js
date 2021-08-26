fd5ced48f70b14c3bb682bee61a75550f3e5d2d5
your name here <your_email_hereroelliga@yahoo.com>
2021-08-26 14:20:44 -0700

    addNumbers

--- Classwork/w8/w8d4/intro_callback_exercises.js
+++ Classwork/w8/w8d4/intro_callback_exercises.js
@@ -37,4 +37,27 @@ class Clock {
     }
   }
   
-  const clock = new Clock();
+  // const clock = new Clock();
+
+  const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function addNumbers(sum, numsLeft, completionCallback){
+  if(numsLeft > 0){
+    reader.question('Enter a number', (usernum) => {
+      const numToAdd = parseInt(usernum);
+      sum += numToAdd;
+      console.log(`partialSum: ${sum}`);
+      addNumbers(sum, numsLeft - 1, completionCallback);
+    })
+  } else {
+    completionCallback(sum);
+    reader.close()
+  }
+}
+
+// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
