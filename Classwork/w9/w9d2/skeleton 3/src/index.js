4af6097ee90e2f788ae061d062ed1fc7fcc6c4de
Kent Zhang <kentzhang31@yahoo.com>
2021-08-31 14:59:38 -0700

    Finished grid set up

--- Classwork/w9/w9d2/skeleton 3/src/index.js
+++ Classwork/w9/w9d2/skeleton 3/src/index.js
@@ -1,6 +1,8 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view");
+const Game = require("../ttt_node/game");
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const newGame = new Game();
+  const ttt = document.querySelector('.ttt');
+  const newView = new View(newGame, ttt);
 });
--- Classwork/w9/w9d2/skeleton 3/src/ttt-view.js
+++ Classwork/w9/w9d2/skeleton 3/src/ttt-view.js
@@ -1,7 +1,38 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    const ul = document.createElement("ul");
+    ul.style.display = "flex";
+    ul.style.width = "456px";
+    ul.style.flexWrap = "wrap";
+
+    for(let i = 0; i < 3; i++) {
+      for(let j = 0; j < 3; j++) {
+        const li = document.createElement("li");
+        li.setAttribute("value", [i, j]);
+        li.style.listStyleType = "none";
+        li.style.border = "1px solid #000000";
+        li.style.width = "150px";
+        li.style.height = "150px";
+        li.style.background = "#808080";
+        li.addEventListener("mouseover", function (event) {
+          event.target.style.background = "#ffff00";
+
+          setTimeout(function() {
+            event.target.style.background = "#808080";
+          }, 300);
+        });
+        ul.append(li);
+      }
+    }
+    this.el.append(ul);
+    
+  }
   
   bindEvents() {}
 
