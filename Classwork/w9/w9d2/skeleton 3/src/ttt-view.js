7994ba3c7f2c5a18df2393a16950f7103d97200b
Alex Crooks <alexcrooks2@gmail.com>
2021-08-31 17:05:01 -0700

    Tic Tac Toe Done

--- Classwork/w9/w9d2/skeleton 3/src/ttt-view.js
+++ Classwork/w9/w9d2/skeleton 3/src/ttt-view.js
@@ -7,39 +7,56 @@ class View {
 
   setupBoard() {
     const ul = document.createElement("ul");
-    ul.style.display = "flex";
-    ul.style.width = "456px";
-    ul.style.flexWrap = "wrap";
-
+    let that = this;
     for(let i = 0; i < 3; i++) {
       for(let j = 0; j < 3; j++) {
         const li = document.createElement("li");
-        li.setAttribute("value", [i, j]);
-        li.style.listStyleType = "none";
-        li.style.border = "1px solid #000000";
-        li.style.width = "150px";
-        li.style.height = "150px";
-        li.style.background = "#808080";
-        li.addEventListener("mouseover", function (event) {
-          event.target.style.background = "#ffff00";
-
-          setTimeout(function() {
-            event.target.style.background = "#808080";
-          }, 300);
+        li.setAttribute("value", `${i} ${j}`);
+        li.addEventListener("click", function(event) {
+            event.target.style.background = "#ffffff";
+            that.makeMove(li)            
         });
         ul.append(li);
       }
     }
     this.el.append(ul);
-    
+  }
+
+  makeMove(square) {
+    if(square.innerHTML) {
+      alert("Move was invalid")
+    }
+    else {
+      square.innerHTML = this.game.currentPlayer.toUpperCase();
+      if(this.game.currentPlayer === 'x') {
+        square.style.color = "#db0902";
+      }
+      else {
+        square.style.color = "#2802bf";
+      }
+      const moveToPlay = square.getAttribute("value");
+      this.game.playMove(moveToPlay.split(" "));
+    }
+    const chickenDinner = this.game.winner();
+    if(chickenDinner) {
+      this.winner()
+    }
   }
   
-  bindEvents() {}
+  winner() {
+    this.game.swapTurn();
+    const winningMessage = document.createElement("h1")
+    winningMessage.innerHTML = `CONGRATULATIONS ${this.game.currentPlayer.toUpperCase()}, YOU WON!!!!`
+    this.el.append(winningMessage);
+    const elements = this.el.querySelector('ul')
+    for(let i = 0; i < elements.children.length; i++) {
+      if(elements.children[i].innerHTML === this.game.currentPlayer.toUpperCase()) {
+        elements.children[i].style.color = "#ffffff"
+        elements.children[i].style.background = "#099e02"
+      }
+    }
+  }
 
-  handleClick(e) {}
-
-  makeMove(square) {}
-
-}
+}// end of the class
 
 module.exports = View;
